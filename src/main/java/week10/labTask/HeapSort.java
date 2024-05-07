ba88ca3e8e298b95e10f7e71ebe48cb0b1d74eff
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2024-05-07 09:22:37 +0200

    Added Week 10 materials.

---
+++ src/main/java/week10/labTask/HeapSort.java
@@ -0,0 +1,11 @@
+package week10.labTask;
+
+import week10.BinaryHeap;
+
+public class HeapSort {
+
+    /* Heap sort algorithm for generic-type data */
+    public static <Data extends Comparable<Data>> void sort(Data[] elements) {
+        // your code here
+    }
+}
---
+++ src/test/java/week10/HeapSortTest.java
@@ -0,0 +1,2 @@
+package week10;public class HeapSortTest {
+}
