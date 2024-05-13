c3835a1e336d2aebcc800cbab1b7451d449094d4
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2024-05-13 16:06:17 +0200

    Added Homework 1 files.

---
+++ src/main/java/homework1/BinarySearch.java
@@ -0,0 +1,11 @@
+package homework1;
+
+public class BinarySearch {
+    // track the number of iterations needed for a search to complete
+    public static int numSteps = 0;
+
+    public static int search(Entry[] entries, String searchableName) {
+        // implement the actual logic (remove next line)
+        return -1;
+    }
+}
---
+++ src/main/java/homework1/MergeSort.java
@@ -0,0 +1,9 @@
+package homework1;
+
+import java.util.Comparator;
+
+public class MergeSort {
+    public static void sort(Entry[] students) {
+        // implement the actual logic
+    }
+}
